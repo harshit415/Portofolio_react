fddfb17d155faf5eb6974270ff9a3f7041761bfa
harsh <9955stsk@gmail.com>
2025-03-17 11:40:29 +0530

    pro commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layout from './Components/Layout'
 import Home from './Components/Home'
 import About from './Components/About'
-import './Components/style.css'
 import Work from './Components/Work'
 
 function App() {
